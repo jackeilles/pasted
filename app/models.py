9072613118e7ec0477eb8f0e57255e88d47db87a
Jack Eilles <jack@eilles.xyz>
2024-06-12 19:48:46 +0100

    Config DB

--- app/models.py
+++ app/models.py
@@ -16,7 +16,15 @@ class Files(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     filename: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     mimetype: so.Mapped[str] = so.mapped_column(sa.String(16))
+    timestamp: so.Mapped[datetime.datetime] = so.mapped_column(sa.DateTime, default=datetime.datetime.now)
+    owner_id: so.Mapped[int] = so.mapped_column(sa.Integer, sa.ForeignKey('user.id'))
     
+class ShortURL(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    url: so.Mapped[str] = so.mapped_column(sa.String(128), index=True, unique=True)
+    short: so.Mapped[str] = so.mapped_column(sa.String(8), index=True, unique=True)
+    created: so.Mapped[datetime.datetime] = so.mapped_column(sa.DateTime, default=datetime.datetime.now)
+    owner_id: so.Mapped[int] = so.mapped_column(sa.Integer, sa.ForeignKey('user.id'))
 
 @login.user_loader
 def load_user(id):
